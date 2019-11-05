92c894be9a51538d1e444a7f81ddf9ec768bfacb
Lab User <LabUser@labs.salina.k-state.edu>
2019-11-05 10:37:40 -0600

    sequelize, config, models

---
+++ expressSQL/.config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "development": {
+    "username": "uxyipsjw",
+    "password": 'ANIovOniaBKoqr9_2THWJczO76HKt0Mp',
+    "database": "uxyipsjw",
+    "host": "salt.db.elephantsql.com",
+    "dialect": "postgres",
+    "port": 5432,
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
